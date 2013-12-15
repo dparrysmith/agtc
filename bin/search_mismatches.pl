545745df0b6dca8fa03bdabd01869241fde5a426
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2013-12-15 10:12:48 +0000

    Implemented search_mismatches and supporting methods
    Deals with large files by processing a line at a time
    and merging the results

---
+++ bin/search_mismatches.pl
@@ -0,0 +1,21 @@
+use strict;
+use kmer;
+
+
+my $probe = shift @ARGV;
+my $mismatch_tolerance = shift @ARGV;
+my $sequence_file_name = shift @ARGV;
+
+my $k = kmer->new;
+
+#my $mismatch_strs = $k->search_seq_mismatch( $probe, $mismatch_tolerance, $sequence );
+my $mismatch_strs = $k->search_seq_mismatch_file( $probe, $mismatch_tolerance, $sequence_file_name );
+
+print $probe . "\n\n";
+if ( @$mismatch_strs ) {
+    foreach my $match ( @$mismatch_strs ) {
+        my $ham = $k->hamming( $match );
+        last if  $ham > $mismatch_tolerance;
+        printf $k->prettify_match( $match ) . ' => ' . $ham  . "\n";
+    }
+}
--- lib/kmer.pm
+++ lib/kmer.pm
@@ -1,8 +1,11 @@
 package kmer;
 use Moose;
 
-=head
-Calculate the most frequent k-word strings in an input string
+=head agtc
+Calculate the most frequent k-word strings in an input string.
+
+Handles mismatches so that word-length substrings can be returned with up to
+mismatch_value mismatches distributed across the length of the string. 
 
 DJP-S Nov-2013
 =cut
@@ -24,6 +27,12 @@ has mismatch_limit => (
 );
 
 our @probe_array = ();
+
+=head kmer
+Given sequence-string word-length
+Returns a list of all strings of length word-length in the supplied string
+
+=cut
 
 sub kmer {
     my $self = shift;
@@ -51,6 +60,71 @@ sub kmer {
     my @frequent_strs;
     foreach my $k_key ( @sorted_keys ) { # now any others at the same frequency
         if ( $kwords{ $k_key } == $max_freq ) {
+            push @frequent_strs, $k_key;
+        }
+        else {
+            last;
+        }
+    }
+    return \@frequent_strs;
+}
+
+=head kmer_mismatch
+Given sequence_string probe_string mismatch_value hash_reference
+
+Returns the list of frequently occurring strings matching the probe_string
+with a maximum number of mismatches given by the mismatch_value
+
+The final hash reference is a pointer to the hash that will contain the growing
+list of candidates.
+=cut
+
+sub kmer_mismatch {
+    my $self = shift;
+
+    my $string = shift;
+    my $candidates = shift;
+
+    my $k_value = $self->len_probe_seq;
+    my $mismatch_value = $self->mismatch_limit;
+
+    my $start = 0;
+    my $end = length( $string ) - $k_value + 1;
+
+
+    for ( my $offset = $start; $offset < $end; $offset++) {
+        my $candidate = substr( $string, $offset, $k_value);
+        my $hamd = $self->hamming( $candidate );
+        if ( $hamd <= $mismatch_value ) {
+            $candidates->{ $candidate } ++;
+        }
+    }
+    return ;
+}
+
+
+=head seq_frequency
+Given a hash of sequence keys
+
+Returns a list of sorted keys based on hamming distance from the probe
+=cut
+
+sub seq_frequency {
+    my $self = shift;
+    my $kwords = shift; # ref to a hash
+$DB::single=1;
+# Sort the keys on their frequency (value) fields
+    my @sorted_keys = sort {
+        $kwords->{$b} <=> $kwords->{$a}
+    } keys %{$kwords};
+    my @vals = @{$kwords}{@sorted_keys};
+
+    my $max_freq =  $kwords->{ $sorted_keys[0] }; # most frequent k_word score;
+#    die "No subsequences exist with frequency greater than 1\n" if $max_freq == 1;
+
+    my @frequent_strs;
+    foreach my $k_key ( @sorted_keys ) { # now any others at the same frequency
+        if ( $kwords->{ $k_key } == $max_freq ) {
             push @frequent_strs, $k_key;
         }
         else {
@@ -88,12 +162,68 @@ sub search_seq_mismatch {
     $self->mismatch_limit( $mismatch_tolerance );
 
     #generate all the subsequences the same length as the probe
-    my $frequent_strs = $self->kmer( $sequence, $self->len_probe_seq );
+    my $frequent_strs = $self->kmer_mismatch( $sequence, $self->len_probe_seq );
 
     my @sorted_strs = sort { $self->mismatches } @{$frequent_strs};
 
     return \@sorted_strs;
 }
+
+=head search_seq_mismatch_file
+Given a probe_sequence, mismatch_value and file_name
+
+Return a list of candidate sequences within the mismatch threshold, sorted
+from min to max hamming edit distance against the probe sequence.
+
+The file should be in Fasta format. The first line will be used as the
+title data for the sequence.
+
+This method is designed to handle large files effectively (if not efficiently!)
+=cut
+
+sub search_seq_mismatch_file {
+    my $self = shift;
+$DB::single=1;
+    my $probe = shift;
+    my $mismatch_tolerance = shift;
+    my $sequence_file = shift;
+
+    open( my $seq_fh, "<", $sequence_file )
+        or die "cannot open < $sequence_file: $!";
+
+    my $title = <$seq_fh> ; 
+    chomp $title;
+    chop($title) if ($title =~ m/\r$/);
+
+    $self->probe_seq( $probe );
+    $self->array_probe_seq(); # sets len_probe_seq
+    $self->mismatch_limit( $mismatch_tolerance );
+    my $residual_length = $self->len_probe_seq - 1; # The amount of overlap required
+    # to provide a seamless join for word_length words.
+
+
+    #generate all the subsequences the same length as the probe
+    my %candidates;
+    my $residual = '';
+    while (defined( my $line = <$seq_fh>) ) { # some loop to go through the file for portions of the sequence to send in to kmer_mismatch
+        chomp $line;
+        chop($line) if ($line =~ m/\r$/);
+        my $sequence = $residual . $line;
+        $residual = substr( $sequence, -1 * $residual_length); # sets that up for next time round
+        $self->kmer_mismatch( $sequence, \%candidates);
+    }
+
+    my $frequent_strs = $self->seq_frequency( \%candidates );
+    my @sorted_strs = sort { $self->mismatches } @{$frequent_strs};
+
+    return \@sorted_strs;
+}
+
+=head mismatches
+Given globals $a and $b
+
+Returns the hamming edit distance between the two strings
+=cut
 
 sub mismatches {
     my $self = shift;
@@ -104,6 +234,11 @@ sub mismatches {
     # order the list is ascending numeric order
     return $ham_distance_a - $ham_distance_b;
 }
+
+=head hamming
+Given a candidate sequence
+Returns the hamming edit distance with the probe sequence stored in the global probe_array
+=cut
 
 sub hamming { # input strings are assumed to be of the same length
     my $self = shift;
